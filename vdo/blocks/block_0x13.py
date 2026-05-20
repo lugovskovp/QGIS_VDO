82a6b811d55660d73f481193465f34e7006c2558
Paul Lugovskov <p.lugovskov@gmail.com>
2026-05-20 12:18:14 +0300

    feat: add rus0x34 example

--- vdo/blocks/block_0x13.py
+++ vdo/blocks/block_0x13.py
@@ -42,8 +42,12 @@ if __name__ == '__main__':
     fpathbmw = 'c:\\DIY\\VDO\\db_src\\bmw34-2010\\DB\\DB_0'
     vdobmv = VDO_FILE(fpathbmw)
 
+    fpathRu = 'c:\\DIY\\VDO\\db_src\\ru_2013\\ru\\carindb'
+    vdoRu = VDO_FILE(fpathRu)
+
     vdo = vdobmv
     vdo = vdo34
+    vdo = vdoRu
     bla = BLADDR(b'\x00\x00\x00\x01', vdo)
 
     #
@@ -60,14 +64,17 @@ if __name__ == '__main__':
     interest = BYTESTRUCT(inf.read(0x2c, 0x10))
     
     print()
-    # print(inf.str_information)
+    print(inf.str_information)
     # bnl : 1.  name:                     bnl_nok_nok_12q4_20130128p.
     #       2.  content:                  benelux (benelux)
     #       3.  oem:                      nok
-    # rus : 1.  name:                     bal_ad_nod_45051_20050210k.
+    # old : 1.  name:                     bal_ad_nod_45051_20050210k.
     #       2.  content:                  baltics (europe)
     #       3.  oem:                      aftersales
     # bmw : 1.  name:                     rus_hw_ntq_opel_09q4_20100305p21.
+    #       2.  content:                  russia (russia)
+    #       3.  oem:                      opel
+    # rus : 1.  name:                     rus_hw_ntq_opel_11q4_20120416p23.
     #       2.  content:                  russia (russia)
     #       3.  oem:                      opel
 

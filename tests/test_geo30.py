04393fd20abe87ab1cf3389f4e3aa492ba7890dd
Paul Lugovskov <p.lugovskov@gmail.com>
2026-05-22 14:04:54 +0300

    test:

---
+++ tests/test_geo30.py
@@ -0,0 +1,55 @@
+
+from vdo.datatypes import VDO_FILE
+
+
+def get_vdo():
+    fpath30 = 'c:\\DIY\\VDO\\db_src\\NAV_DB\\carindb'
+    vdo30 = VDO_FILE(fpath30)       # noqa: F841
+
+    fpath34 = 'c:\\DIY\\VDO\\db_src\\1. BNL_13_14\\carindb'
+    vdo34 = VDO_FILE(fpath34)       # noqa: F841
+
+    fpathRu = 'c:\\DIY\\VDO\\db_src\\ru_2013\\ru\\carindb'
+    vdoRu = VDO_FILE(fpathRu)       # noqa: F841
+
+    fpathbmw = 'c:\\DIY\\VDO\\db_src\\bmw34-2010\\DB\\DB_0'
+    vdobmv = VDO_FILE(fpathbmw)       # noqa: F841
+
+    vdo = vdobmv
+    #vdo = vdo34
+    #vdo = vdoRu
+    vdo = vdo30
+    return vdo
+
+
+#--------------------------------------
+
+vdo = get_vdo()
+# ru 0x1E6EA000 === 03cdd401 14 0000 [14:MAP__05k200]
+geo_bl = vdo.get_block(0x1E6EA000)
+
+ss = geo_bl.read(geo_bl.toc['li_str'].ptr, 16)  # облом 0x7c - b'\x00\x80\x02\x00atlantic oce' # noqa: E501
+st = geo_bl.read_tstr(geo_bl.toc['li_str'].ptr)
+#
+
+pass
+
+"""
+# noqa: E501, W291
+print_about_coord(50.9619376, 11.256128, "Nohra");
+
+bl_addr(0x00D65801);	// scale 0
+bl_addr(0x00958E01);	// scale 1  0x06; 50.835602 N, 11.059422 E - 51.307457 N, 11.531281 E
+/*bl_addr(0x02B54D04);	// scale 2
+bl_addr(0x028D2904);	// scale 3
+
+bl_addr(0x02644E04);	// scale 4; 0x03; 50.835602 N, 11.059422 E - 51.307457 N, 11.531281 E
+bl_addr(0x03CF9A09);	// scale 5; 0x14; 50.363743 N,  7.756413 E - 52.251175 N, 11.531281 E
+bl_addr(0x03965005);	// scale 6; 0x15; 50.363743 N, 10.587563 E - 51.307457 N, 11.531281 E
+bl_addr(0x02F6F404);	// scale 7; 0x16; 50.835602 N, 11.059422 E - 51.307457 N, 11.531281 E 
+//bl_addr(0x00000000);	// scale 8;   
+bl_addr(0x03C4A30B);	// scale 9; 0x1c; 50.363743 N,  9.643848 E - 52.251175 N, 11.531281 E
+bl_addr(0x03D33D07);	// scale 10;0x1d; 50.363743 N,  7.756413 E - 52.251175 N, 11.531281 E
+//bl_addr(0x00000000);	// scale 11;0x1e;
+
+"""
--- tests/test_get_block.py
+++ tests/test_get_block.py
@@ -18,7 +18,7 @@ vdobmv = VDO_FILE(fpathbmw)
 vdo = vdobmv
 #vdo = vdo34
 #vdo = vdoRu
-vdo = vdo30
+#vdo = vdo30
 #--------------------------------------
 
 bl_false = vdo.get_block(100)
@@ -28,7 +28,10 @@ bla = BLADDR(b'\x00\x00\x00\x00', vdo)
 bl_zero = vdo.get_block(bla)
 addr = bl_first.head.bladdr.offset + (bl_first.head.bladdr.segcnt * bl_first.vdo.segsize)   # noqa: E501
 bl_third = vdo.get_block(addr)
-addr = bl_third.offset_next()
+if bl_third:
+    addr = bl_third.offset_next()
+else:
+    addr = bl_first.bladdr_scales.offset
 bl_forths = vdo.get_block(addr)
 
 print(f"{bl_false}\n{bl_first}\n{bl_third}\n{bl_zero}\n{bl_forths}")

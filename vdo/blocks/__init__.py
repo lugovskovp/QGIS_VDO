06ac88595051d7170c97821f8ed4bd379ed3c3b2
Paul Lugovskov <p.lugovskov@gmail.com>
2026-07-09 10:24:19 +0300

    block_0x08 - SCALE_ALMANAC = 0x08

--- vdo/blocks/__init__.py
+++ vdo/blocks/__init__.py
@@ -1,21 +1,22 @@
+# scales and coordinates
+from QGIS_VDO.vdo.blocks.block_0x07 import block_0x07    # noqa  SCALES
+from QGIS_VDO.vdo.blocks.block_0x08 import block_0x08    # noqa  SCALE_ALMANAC = 0x08  
 
-from QGIS_VDO.vdo.blocks.block_0x0B import block_0x0B   # ch_idx country    # noqa: F401
-from QGIS_VDO.vdo.blocks.block_0x0A import block_0x0A   # country   # noqa: F401
+# address info
+from QGIS_VDO.vdo.blocks.block_0x0B import block_0x0B   # noqa # ch_idx country  
+from QGIS_VDO.vdo.blocks.block_0x0A import block_0x0A   # noqa # country   
+from QGIS_VDO.vdo.blocks.block_0x0D import block_0x0D   # noqa # ch_idx city   
+# from QGIS_VDO.vdo.blocks.block_0x0C import block_0x0C  # noqa # city    
 
-from QGIS_VDO.vdo.blocks.block_0x0D import block_0x0D   # ch_idx city    # noqa: F401
-# from QGIS_VDO.vdo.blocks.block_0x0C import block_0x0C  # city    # noqa: F401
-
-from QGIS_VDO.vdo.blocks.block_0x12 import block_0x12   # abstract   # noqa: F401
-from QGIS_VDO.vdo.blocks.block_0x13 import block_0x13   # bibliogr   # noqa: F401
+# overall information
+from QGIS_VDO.vdo.blocks.block_0x12 import block_0x12   # noqa# abstract   
+from QGIS_VDO.vdo.blocks.block_0x13 import block_0x13   # noqa # bibliogr   
 
 # base_geotypes
-from QGIS_VDO.vdo.blocks.block_0x14 import block_0x14    # noqa: F401
-from QGIS_VDO.vdo.blocks.block_0x15 import block_0x15    # noqa: F401
-from QGIS_VDO.vdo.blocks.block_0x16 import block_0x16    # noqa: F401
-from QGIS_VDO.vdo.blocks.block_0x1C import block_0x1C    # noqa: F401
-from QGIS_VDO.vdo.blocks.block_0x1D import block_0x1D    # noqa: F401
-from QGIS_VDO.vdo.blocks.block_0x1E import block_0x1E    # noqa: F401
-# from vdo.blocks.block_0x00 import block_0x00    # noqa: F401
-
-#
-from QGIS_VDO.vdo.blocks.block_0x07 import block_0x07    # noqa: F401
+from QGIS_VDO.vdo.blocks.block_0x14 import block_0x14    # noqa #
+from QGIS_VDO.vdo.blocks.block_0x15 import block_0x15    # noqa #
+from QGIS_VDO.vdo.blocks.block_0x16 import block_0x16    # noqa #
+from QGIS_VDO.vdo.blocks.block_0x1C import block_0x1C    # noqa #
+from QGIS_VDO.vdo.blocks.block_0x1D import block_0x1D    # noqa #
+from QGIS_VDO.vdo.blocks.block_0x1E import block_0x1E    # noqa #
+# from vdo.blocks.block_0x00 import block_0x00    # noqa #
---
+++ vdo/blocks/block_0x08.py
@@ -0,0 +1,38 @@
+"""
+SCALE_ALMANAC = 0x08    # set of map folders 0x9.
+
+Индекс папок с гео-блоками.
+Описывает квадрат (в SCALE), количество итемов - папок (block_0x9),
+дельта координат между папками, сам список папок
+
+block_0x08
+
+//header start
+    BL_HEADER       block;
+// header end, last = 0Ch -1
+   DWORD folder_side_size <format=hex, fgcolor=cYellow,bgcolor=cDkGreen>;
+   //was side_one_square_almanac
+    
+   FOLDER_MAPS folder[block.data.Cnt] <optimize=false>;
+
+}BLOCK_TYPE_0x08
+
+"""
+
+from QGIS_VDO.vdo.consts import struct_UINT  # struct_WORD  #
+from QGIS_VDO.vdo.block_base import block_base
+from QGIS_VDO.vdo.datatypes import BLADDR       # BYTESTRUCT,
+
+
+OFFSET_LIST_FOLDEFS = 0x08
+OFFSET_FOLDER_SIZE = 0x0c
+
+
+class block_0x08(block_base):
+    """
+    """
+    def __init__(self, bl_addr: BLADDR) -> None:
+        super().__init__(bl_addr)
+        self.side = struct_UINT.unpack(OFFSET_FOLDER_SIZE)
+        self.li_folders = self.list(OFFSET_LIST_FOLDEFS)
+        

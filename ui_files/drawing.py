d2efa1d4c1890ba6aa9c868f8f553e262a2c3049
Paul Lugovskov <p.lugovskov@gmail.com>
2026-07-22 16:10:30 +0300

    refactor: _DrawArea, add renderer  elif layerName == NAME_LAYER_ALMANACS:

--- ui_files/drawing.py
+++ ui_files/drawing.py
@@ -3,39 +3,49 @@
 """
 
 
-# from qgis.core import (Qgis, QgsProject, QgsVectorLayer, QgsField,
-#                        QgsSingleSymbolRenderer, QgsFillSymbol,
-#                        QgsPointXY, QgsRectangle, QgsGeometry, QgsFeature,
-#                        QgsLayerTreeGroup, QgsCoordinateTransform)
 from qgis.core import (Qgis, QgsVectorLayer, QgsPointXY, QgsRectangle,
                        QgsSingleSymbolRenderer, QgsFillSymbol, QgsFeature,
-                       QgsGeometry)
+                       QgsFeatureRequest, QgsGeometry)
 from qgis.PyQt.QtGui import QColor
+from qgis.PyQt.QtCore import Qt
 
-from QGIS_VDO.vdo.consts import NAME_LAYER_GLOBAL_BOUNDS, NAME_LAYER_ALMANACS
+from QGIS_VDO.vdo.consts import (NAME_LAYER_GLOBAL_BOUNDS, NAME_LAYER_ALMANACS)
 
 
 def _DrawArea(area, area_name: str, layer: QgsVectorLayer) -> None:
     """
-    Рисует прямоугольник в слое layer.
+    Рисует прямоугольник в слое layer, в котором должен быть атрибут name.
     Args:
         area (coord lb, coord rt)
-        area_name: str
+        area_name: str имя добавляемой area
         layer: QgsVectorLayer Qgis.GeometryType.Polygon:
     """
-    # если есть объект с таким именем - возврат
-    # Перебираем все объекты слоя
-    for feature in layer.getFeatures():
-        # feature.id() — уникальный внутренний номер объекта в QGIS
-        # feature.attributes() — список всех текстовых/числовых значений в таблице
-        if feature.attribute('name') == area_name:
-            return
-        print(f"ID: {feature.id()} | {feature.attribute('name')} | Данные: {feature.attributes()}")  # noqa
-
+    field_name_index = layer.fields().indexOf('name')
+    # если в слое нет атрибута name - возврат
+    if field_name_index == -1:
+        return
     # если слой не poligone - возврат
     if not layer or layer.geometryType() != Qgis.GeometryType.Polygon:
-        print("Ошибка: Пожалуйста, выберите ПОЛИГОНАЛЬНЫЙ слой для прямоугольника!")
+        # print("Ошибка: Пожалуйста, выберите ПОЛИГОНАЛЬНЫЙ слой для прямоугольника!")
         return
+    # если есть объект с таким именем - возврат
+    # Формируем выражение и получаем ID всех подходящих объектов - быстрее перебора
+    expression = f"\"name\" = '{area_name}'"
+    matching_features = layer.getFeatures(QgsFeatureRequest().setFilterExpression(expression))  # noqa
+    ids = [f.id() for f in matching_features]
+    if ids:
+        return
+        # matching_features = layer.getFeatures(QgsFeatureRequest().setFilterExpression(expression))  # noqa
+        # ids = [f.id() for f in matching_features]
+
+    # Перебираем все объекты слоя
+    # for feature in layer.getFeatures():
+    #     # feature.id() — уникальный внутренний номер объекта в QGIS
+    #     # feature.attributes() — список всех текстовых/числовых значений в таблице
+    #     kk = feature.attributes()
+    #     if feature.attribute('name') == area_name:
+    #         return
+        # print(f"ID: {feature.id()} | {feature.attribute('name')} | Данные: {feature.attributes()}")  # noqa
 
     # координаты точек
     p_lb = QgsPointXY(area[0].lon, area[0].lat)
@@ -46,12 +56,12 @@ def _DrawArea(area, area_name: str, layer: QgsVectorLayer) -> None:
     geom = QgsGeometry.fromRect(rect)
     
     # Создаем новый объект (Feature) и присваиваем ему геометрию
-    feature = QgsFeature()
+    feature = QgsFeature(layer.fields())
     feature.setGeometry(geom)
     
     # Если в слое есть атрибуты, можно задать дефолтные значения (опционально)
-    feature.setAttributes([1, area_name, f"{area[0].__repr__()}, {area[1].__repr__()}"])  # noqa
-
+    feature.setAttribute(field_name_index, area_name)
+   
     # Начинаем редактирование слоя и добавляем объект
     layer.startEditing()
     success = layer.addFeature(feature)
@@ -59,7 +69,7 @@ def _DrawArea(area, area_name: str, layer: QgsVectorLayer) -> None:
     if success:
         layer.commitChanges()    # Сохраняем изменения
         layer.triggerRepaint()   # Обновляем карту
-        print("Прямоугольник успешно добавлен на слой!")
+        # print("Прямоугольник успешно добавлен на слой!")
     else:
         layer.rollBack()     # Отменяем правки в случае ошибки
         print("Не удалось добавить объект на слой.")
@@ -74,27 +84,38 @@ def getRendererByLayerName(layerName: str) -> QgsSingleSymbolRenderer:
     """
     #
     if layerName == NAME_LAYER_GLOBAL_BOUNDS:
-        # renderer для слоя глобальных границ
+        # renderer для слоя глобальных границ - лесной зелёный
         # Настраиваем стиль (Символогию) Создаем дефолтный символ для полигона
         symbol = QgsFillSymbol.createSimple({'name': 'square'})
         
         # НАСТРОЙКА ЦВЕТА ЗАЛИВКИ (RGBA: Красный, Зеленый, Синий, Альфа/Прозрачность от 0 до 255) # noqa
         # 128 в конце означает 50% прозрачности (0 - полностью прозрачный, 255 - сплошной)  # noqa
-        fill_color = QColor(34, 139, 34, 20)   # Лесной зеленый с 20% прозрачностью
+        fill_color = QColor(34, 139, 34, 10)   # Лесной зеленый с 10% прозрачностью
         symbol.setColor(fill_color)
         
         # НАСТРОЙКА ГРАНИЦЫ
-        symbol.symbolLayer(0).setStrokeColor(QColor(0, 0, 0, 255))  # Черный цвет границы (сплошной) # noqa
+        symbol.symbolLayer(0).setStrokeColor(QColor(0, 40, 0, 255))  # Черный цвет границы (сплошной) # noqa
         symbol.symbolLayer(0).setStrokeWidth(0.6)                   # Толщина границы в миллиметрах  # noqa
         # Доступные стили границы: Qt.SolidLine, Qt.DashLine, Qt.DotLine и т.д.
         
         # НАСТРОЙКА ОБЩЕЙ ПРОЗРАЧНОСТИ СЛОЯ (Альтернативный вариант от 0.0 до 1.0)
         # symbol.setOpacity(0.7) # 70% непрозрачности для всего символа целиком
+
     elif layerName == NAME_LAYER_ALMANACS:
-        # слой альманах карт
-        symbol = 0
+        # слой альманах карт - персиковый
+        # Настраиваем стиль (Символогию) Создаем дефолтный символ для полигона
+        symbol = QgsFillSymbol.createSimple({'name': 'square'})
+        # НАСТРОЙКА ЦВЕТА ЗАЛИВКИ (RGBA: Красный, Зеленый, Синий, Альфа/Прозрачность от 0 до 255) # noqa
+        fill_color = QColor(255, 229, 180, 10)   # Персиковый с 10% прозрачностью
+        symbol.setColor(fill_color)
+        # НАСТРОЙКА ГРАНИЦЫ
+        symbol.symbolLayer(0).setStrokeColor(QColor(255, 129, 80, 255))  # Персиковый цвет границы (сплошной) # noqa
+        symbol.symbolLayer(0).setStrokeWidth(0.6)                   # Толщина границы в миллиметрах  # noqa
+        # Доступные стили границы: Qt.SolidLine, Qt.DashLine, Qt.DotLine и т.д.
+        symbol.symbolLayer(0).setStrokeStyle(Qt.DashLine)
+
     else:
-        raise ValueError(layerName, "неизвестно, _getRendererByName")
+        raise ValueError(layerName, "неизвестный слой, _getRendererByName")
 
     # Применяем настроенный символ к рендереру слоя
     renderer = QgsSingleSymbolRenderer(symbol)

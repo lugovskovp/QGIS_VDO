dff7eb87a662203ccfad165c8504272e965fa1c2
Paul Lugovskov <p.lugovskov@gmail.com>
2026-07-26 12:55:23 +0300

    refactor: Перегенерированный файл, с пустыми переводами

---
+++ i18n/QGIS_VDO_ru.ts
@@ -0,0 +1,351 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
+<context>
+    <name>ConfigurationDialogBase</name>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="30"/>
+        <source>Configure Carindb VDO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="64"/>
+        <source>Display description with the current plugin name beside the icon in the Plugin toolbar.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="67"/>
+        <source>Display the plugin name in the toolbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="167"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;QGIS will try to execute any commands typed here in a shell before reloading the plugin.&lt;/p&gt;
+&lt;p&gt;This can be useful, for example, if you need to copy the new source code into the QGIS plugins directory.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="90"/>
+        <source>Run the commands below before reloading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="103"/>
+        <source>Show Clear recent files action in menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="126"/>
+        <source>Hide non active vdo group on change vdo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="178"/>
+        <source>Note: any %PluginName% will be replaced by the plugin&apos;s name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="198"/>
+        <source>Maximum number of entries in the drop-down menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/ConfigurationDialogBase.ui" line="201"/>
+        <source>Number of recent files in the menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QgisVdoDockwidget</name>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidget.py" line="125"/>
+        <source>Where are no Area_A, Area_B in this Carindb.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidget.py" line="185"/>
+        <source>Load {} fld</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidget.py" line="291"/>
+        <source>Open/create any qgis project and reopen Carindb.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QgisVdoDockwidgetBase</name>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="41"/>
+        <source>Carin C-IQ VDO DB Viewer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="118"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="184"/>
+        <source>DB file information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="213"/>
+        <source>File Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="227"/>
+        <source>----</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="250"/>
+        <source>--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="260"/>
+        <source>Seg Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="273"/>
+        <source>DB rev. </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="287"/>
+        <source>- --- ---</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="297"/>
+        <source>File Path:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="311"/>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="332"/>
+        <source>Area A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="557"/>
+        <source>1N 1E</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="539"/>
+        <source>Left Bottom:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="576"/>
+        <source>Right Top:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="588"/>
+        <source>0N 0E</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="606"/>
+        <source>show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="490"/>
+        <source>Area B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="629"/>
+        <source>Label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="669"/>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="715"/>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="790"/>
+        <source>Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="829"/>
+        <source>GroupBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="987"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="972"/>
+        <source>Topography</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1039"/>
+        <source>RadioButton 0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1046"/>
+        <source>RadioButton 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1053"/>
+        <source>RadioButton 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1060"/>
+        <source>RadioButton 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1067"/>
+        <source>RadioButton 4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1074"/>
+        <source>RadioButton 5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1085"/>
+        <source>RadioButton 6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1092"/>
+        <source>RadioButton 7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1102"/>
+        <source>RadioButton 8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1109"/>
+        <source>RadioButton 9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1116"/>
+        <source>RadioButton 10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1123"/>
+        <source>RadioButton 11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1141"/>
+        <source>POI Categories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1179"/>
+        <source>Delete All Groups Carindb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_files/QgisVdoDockwidgetBase.ui" line="1200"/>
+        <source>Block</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VDOExplorerPlugin</name>
+    <message>
+        <location filename="../vdo_explorer.py" line="124"/>
+        <source>&amp;Carindb VDO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="132"/>
+        <source>Load Carindb ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="138"/>
+        <source>Clear ALL RECENT FILES list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="143"/>
+        <source>Configure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="230"/>
+        <source>Load carindb file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="299"/>
+        <source>Not found: {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="303"/>
+        <source>Not carindb: {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="323"/>
+        <source>Can`t find file.
+Do you want to remove path from recent?
+{}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="347"/>
+        <source>Delete from recent files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="357"/>
+        <source>Do you want to load another carindb?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="358"/>
+        <source>Open another carindb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="346"/>
+        <source>This is not carindb by inner structure.
+Do you want to remove path from recent?
+{}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vdo_explorer.py" line="389"/>
+        <source>Open any Carindb file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

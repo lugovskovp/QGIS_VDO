6cbb63bd70a3c202929c5026fdb44a514e9b166c
Paul Lugovskov <p.lugovskov@gmail.com>
2026-07-27 11:40:16 +0300

    + COORD, - struct_WORD

--- vdo/__init__.py
+++ vdo/__init__.py
@@ -1,3 +1,4 @@
 from QGIS_VDO.vdo.datatypes import VDO_FILE, BLADDR, PTR, LIST, FAR_LIST, CH_IDX, BLSTART  # noqa: F401, E501 (Ignores only "unused import")
 # from QGIS_VDO.vdo.CollapsibleGroupBox import CollapsibleGroupBox, CollapsibleBox  # noqa: F401, E501 (Ignores only "unused import")# noqa: F401, E501 (Ignores only "unused import")
-from QGIS_VDO.vdo.consts import struct_WORD  # noqa
+# from QGIS_VDO.vdo.consts import struct_WORD  # noqa
+from QGIS_VDO.vdo.geotypes import COORD # noqa

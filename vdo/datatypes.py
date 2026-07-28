2eed43111533ffdcc97ae0915cc25cd5455b5043
Paul Lugovskov <p.lugovskov@gmail.com>
2026-07-28 20:03:04 +0300

    VDO_FILE.get_block return block_base

--- vdo/datatypes.py
+++ vdo/datatypes.py
@@ -27,6 +27,7 @@ else:
     ReadableBuffer = bytes
 
 
+from QGIS_VDO.vdo.block_base import block_base
 from .enums import BlockType
 from .consts import struct_WORD, struct_UINT
 from .consts import USHORT_BYTES_CNT, UINT_BYTES_CNT, DOUBLE_BYTES_CNT, ZERO_DWORD, EMPTY_BUFFER
@@ -118,7 +119,7 @@ class VDO_FILE():
             return f.read(size)
         return None
 
-    def get_block(self, addr: Union[int, BLADDR], *args: Any) -> Any | None:
+    def get_block(self, addr: Union[int, BLADDR], *args: Any) -> block_base | None:
         """
         Возвращает экземпляр блока по смещению offset (int) или из структуры BLADDR.
         

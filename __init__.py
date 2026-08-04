41f4f66f1811e7bf1e1fd9595db369bd71cd8022
Paul Lugovskov <p.lugovskov@gmail.com>
2026-08-04 20:50:21 +0300

    license

--- __init__.py
+++ __init__.py
@@ -1,4 +1,21 @@
-''' Carindb VDO
+'''
+Copyright (C) 2026 Lugovskov Pavel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://gnu.org>.
+
+
+Carindb VDO
 A QGIS plugin
 Systeme Guidage Carminat C-IQ navigation database viewer
 
@@ -7,10 +24,14 @@ Systeme Guidage Carminat C-IQ navigation database viewer
         email                : p.lugovskov@gmail.com
         git sha              : https://github.com/lugovskovp/QGIS_VDO
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.'''
+try:
+    import bitarray
+except ImportError:
+    import subprocess
+    import sys
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "bitarray"])
+
+'''
 
 import os
 import sys

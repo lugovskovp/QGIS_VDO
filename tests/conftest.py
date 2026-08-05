ee6f5707eb4b3808c9fb6d5b481e182c37f0b176
Paul Lugovskov <p.lugovskov@gmail.com>
2026-08-05 12:59:32 +0300

    comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 import os
-import sys
+# import sys
 # from unittest.mock import MagicMock
 
 from fixtures import FIXTURES_DIR
 
 # Принудительно добавляем родительскую директорию в пути поиска модулей
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # # 1. Жесткая настройка путей для всех уровней вложенности
 # BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))

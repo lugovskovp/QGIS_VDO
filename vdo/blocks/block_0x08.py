d0d7efbd3b4040f57916fab5d70baf569c6d497f
Paul Lugovskov <p.lugovskov@gmail.com>
2026-08-07 15:55:23 +0300

    #50 переписан на slots, 32bit ariphmetic, скорость

--- vdo/blocks/block_0x08.py
+++ vdo/blocks/block_0x08.py
@@ -21,12 +21,13 @@ block_0x08
 
 """
 
-from typing import Iterator
+from typing import Iterator, Tuple
 
 from QGIS_VDO.vdo.block_base import block_base
 from QGIS_VDO.vdo.blocks import block_0x09
 from QGIS_VDO.vdo.datatypes import BLADDR
-from QGIS_VDO.vdo.geotypes import COORD
+from QGIS_VDO.vdo.geotypes import COORD  # , MULCOORD
+from QGIS_VDO.vdo.consts import struct_UINT
 
 
 OFFSET_LIST_FOLDEFS = 0x08
@@ -35,149 +36,142 @@ OFFSET_FOLDER_SIZE = 0x0c
 
 class block_0x08(block_base):
     """
-    0x08    LIST    li_folders  ptr_cnt на BLADDR | 0
-    0x0c    DWORD    side    размер приращения _hlat на следующий folder
-    0x10    [BLADDR] - массив на папки-индексы гео-блоков
+    0x08    LIST     li_folders  ptr_cnt на BLADDR | 0
+    0x0c    DWORD    side        размер приращения _hlat на следующий folder
+    0x10    [BLADDR]             массив на папки-индексы гео-блоков
     """
 
-    def __init__(self, bl_addr: BLADDR, origin: COORD, max: COORD):
-        """
-        Args:
-            bl_addr: BLADDR
-            origin: COORD - left bottom
-            max: COORD - right top
-        """
+    __slots__ = (
+        'li_items',
+        'item_side',
+        'origin_hlon',
+        'origin_hlat',
+        'qty_y',
+        'qty_x'
+    )
+
+    def __init__(self, bl_addr: BLADDR, origin: COORD, max: COORD) -> None:
         super().__init__(bl_addr)
 
-        # item - bladdr value map block
         self.li_items = self.read_list(OFFSET_LIST_FOLDEFS)
         self.item_side = self.uint(OFFSET_FOLDER_SIZE)
         
-        self.origin = origin      # "начало" координат, left bottom
-        self.qty_y = int((max._hlatitude - origin._hlatitude) / self.item_side)
-        self.qty_x = int((max._hlongitude - origin._hlongitude) / self.item_side)
-        pass
+        # Сохраняем сырые uint32 значения для корректной CarInDB-математики
+        self.origin_hlon = origin._hlon
+        self.origin_hlat = origin._hlat
+        
+        # Вычисляем размеры сетки, сохраняя поведение беззнаковых переполнений
+        self.qty_y = (max._hlatitude - origin._hlatitude) // self.item_side
+        self.qty_x = (max._hlongitude - origin._hlongitude) // self.item_side
 
     def items_cnt(self) -> int:
-        """
-        Возвращает количество уникальных итемов
-        """
-        finded_early = []
-        for offset in range(self.li_items.ptr,
-                            self.li_items.ptr + self.li_items.cnt * BLADDR.size,
-                            BLADDR.size):
-            val = self.uint(offset)
-            if not val:
-                continue
-            if val in finded_early:
-                continue
-            finded_early.append(val)
+        """Возвращает количество уникальных итемов за O(N) через хеш-сет."""
+        finded_early = set()
+        raw_buffer = self._raw
+        step = BLADDR.size
+        start_ptr = self.li_items.ptr
+        end_ptr = start_ptr + self.li_items.cnt * step
+
+        for offset in range(start_ptr, end_ptr, step):
+            val = struct_UINT.unpack_from(raw_buffer, offset)[0]
+            if val:
+                finded_early.add(val)
+                
         return len(finded_early)
 
-    def get_items(self) -> Iterator[tuple]:
+    def get_items(self) -> Iterator[Tuple[int, COORD, COORD]]:
+        """Генератор валидных итемов с оригинальными объектами COORD.
+        
+        Возвращает:
+            tuple: (bla_val, coord_lb, coord_rt)
         """
-        Генератор валидных итемов с координатами COORD lb, rt
-        Returns:
-            res = (bla_val, coord_lb, coord_rt): tuple
-                bla_val: int - значение bladdr Folders
-                coord_lb: COORD left bottom
-                coord_rt: COORD right top
-        """
-        finded_early = []        # ранее ptr уже был найден
-        step = BLADDR.size       # единица приращения
-        # "координаты" в квадрате ареа
-        for x in range(self.qty_x):
-            # в файле перебор по вертикали, потом по Х
-            for y in range(self.qty_y):
-                curr_item = y + x * self.qty_x
-                if curr_item >= self.li_items.cnt:
-                    # количество итемов может быть меньше квадрата стороны
+        finded_early = set()
+        step = BLADDR.size
+        base_ptr = self.li_items.ptr
+        total_cnt = self.li_items.cnt
+        q_y = self.qty_y
+        q_x = self.qty_x
+        side = self.item_side
+        raw_buffer = self._raw
+
+        for x in range(q_x):
+            # Ваша оригинальная индексация по qty_x
+            x_offset = x * q_x
+            
+            # Рассчитываем долготу (X) с учетом uint32 специфики
+            hex_lon = (self.origin_hlon + x * side) & 0xFFFFFFFF
+            hex_lon_next = (hex_lon + side) & 0xFFFFFFFF
+
+            for y in range(q_y):
+                curr_item = y + x_offset
+                if curr_item >= total_cnt:
                     break
-                offset = self.li_items.ptr + step * curr_item  # noqa
-                bla_val = self.uint(offset)
-                if not bla_val:
-                    # следующий, если bla_val == 0
-                    continue
-                # а вообще бывают которые занимают 2 и/или 4 места?
-                if bla_val in finded_early:
-                    # если попали хоть раз сюда, то надо допереписать по примеру 0х09
-                    raise ValueError(finded_early, finded_early)
+
+                offset = base_ptr + step * curr_item
+                bla_val = struct_UINT.unpack_from(raw_buffer, offset)[0]
                 
-                # ок, найден новый, координаты углов
-                (coord_lb, coord_rt) = self.get_xy_area(x, y)
-                # res = (bla_val, coord_lb, coord_rt)
+                if not bla_val:
+                    continue
+                    
+                if bla_val in finded_early:
+                    raise ValueError(f"Дубликат bla_val {bla_val} обнаружен в сетке папок")
+                
+                finded_early.add(bla_val)
+
+                # Рассчитываем широту (Y) с маской 0xFFFFFFFF для имитации Си-переполнения
+                hex_lat = (self.origin_hlat + y * side) & 0xFFFFFFFF
+                hex_lat_next = (hex_lat + side) & 0xFFFFFFFF
+
+                # Создаем тяжелые COORD только для валидных элементов (после всех continue)
+                coord_lb = COORD(hex_lon, hex_lat)
+                coord_rt = COORD(hex_lon_next, hex_lat_next)
+
                 yield (bla_val, coord_lb, coord_rt)
-        pass
 
     def find_by_coord(self, srch: COORD) -> BLADDR | None:
-        """
-        Поиск блока КАРТЫ, в который попадают координаты, или None
-        """
-        # проверка, что srch в пределах координат блока
-        max_hlatitude = self.origin._hlatitude + self.qty_y * self.item_side
-        max_hlongitude = self.origin._hlongitude + self.qty_x * self.item_side
-        if srch._hlatitude < self.origin._hlatitude or srch.lat > max_hlatitude \
-           or srch._hlongitude < self.origin._hlongitude or srch.lon > max_hlongitude:
-            # не попал в квадрат lb-rt
-            print(f"bl_0x08: No way: {srch} not in {self.area}")
+        """Поиск подблока карты, в который попадают координаты."""
+        side = self.item_side
+        
+        max_hlatitude = self.origin_hlat + self.qty_y * side
+        max_hlongitude = self.origin_hlon + self.qty_x * side
+        
+        if (srch._hlatitude < self.origin_hlat or srch._hlatitude > max_hlatitude
+                or srch._hlongitude < self.origin_hlon or srch._hlongitude > max_hlongitude):
             return None
-        # расчет offset для srch : _hlongtitude - SIGNED!
-        # delta_hlon_x = (srch._hlongtitude - self.origin._hlongtitude) / self.item_side
-        # delta_hlat_y = (srch._hlatitude - self.origin._hlatitude) / self.item_side
-        delta_x = int((srch._hlongitude - self.origin._hlongitude) / self.item_side)
-        delta_y = int((srch._hlatitude - self.origin._hlatitude) / self.item_side)
-        # Если есть такой блок в итемах
-        if (bladdr_folder_maps := self.get_xy_item(delta_x, delta_y)) is None:
+
+        delta_x = (srch._hlongitude - self.origin_hlon) // side
+        delta_y = (srch._hlatitude - self.origin_hlat) // side
+        
+        bladdr_folder_maps = self.get_xy_item(delta_x, delta_y)
+        if bladdr_folder_maps is None:
             return None
-        # координаты углов полученного итема
-        lb: COORD
-        rt: COORD
-        (lb, rt) = self.get_xy_area(delta_x, delta_y)
-        # bladdr карты ищем в 0x09
+            
+        lb, rt = self.get_xy_area(delta_x, delta_y)
         folder_maps: block_0x09 = self.vdo.get_block(bladdr_folder_maps, lb, rt)
-        # res = folder_maps.find_by_coord(srch)
         return folder_maps.find_by_coord(srch)
 
-    def get_xy_area(self, x: int, y: int) -> tuple[COORD, COORD]:
-        """
-        Координаты lb, rt area x, y
-        значения x, y ОБЯЗАНЫ быть 0..qty_x
-        Returns:
-            tuple[left_bottov, right_top]
-                left_bottom: COORD
-                right_top: COORD
-        """
-        # left bottom
-        # Долгота (Lng) E/W - x
-        hex_lon = self.origin._hlongitude + x * self.item_side
-        # Широта (Lat) N/S - y
-        hex_lat = self.origin._hlatitude + y * self.item_side
-        coord_lb = COORD(hex_lon, hex_lat)
-
-        # right top
-        # if (lat1 := (lat0 + self.delta_degree)) > 85:
-        #     lat1 = 85
-        hex_lat += self.item_side
-        hex_lon += self.item_side
-        coord_rt = COORD(hex_lon, hex_lat)
-
-        return (coord_lb, coord_rt)
+    def get_xy_area(self, x: int, y: int) -> Tuple[COORD, COORD]:
+        """Возвращает оригинальные объекты COORD lb, rt для ячейки x, y."""
+        side = self.item_side
+        hex_lon = (self.origin_hlon + x * side) & 0xFFFFFFFF
+        hex_lat = (self.origin_hlat + y * side) & 0xFFFFFFFF
+        
+        return COORD(hex_lon, hex_lat), COORD((hex_lon + side) & 0xFFFFFFFF, (hex_lat + side) & 0xFFFFFFFF)
 
     def get_xy_item(self, x: int, y: int) -> BLADDR | None:
-        """
-        Вернуть item
-        Args:
-            x, y: int "координаты" в "квадрате" итемов
-        Returns:
-            block_0x09: BLADDR, item self
-        """
-        item_num = y + x * self.qty_y
-        offset = self.li_items.ptr + item_num * BLADDR.size
-        # bl_folder_val = self.uint(offset)
-        res = self.vdo.get_bladdr(self.uint(offset))
-        if res.isZero:
+        """Вернуть адрес папки по индексам сетки."""
+        item_num = y + x * self.qty_x
+        if item_num >= self.li_items.cnt:
             return None
-        return res
+            
+        offset = self.li_items.ptr + item_num * BLADDR.size
+        bladdr_val = struct_UINT.unpack_from(self._raw, offset)[0]
+        
+        if not bladdr_val:
+            return None
+            
+        res = self.vdo.get_bladdr(bladdr_val)
+        return None if res.isZero else res
 
         
 # All block tests in block_0x07

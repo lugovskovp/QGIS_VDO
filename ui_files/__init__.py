e93080ab583af79a542606336b7208e72829d8fa
Paul Lugovskov <p.lugovskov@gmail.com>
2026-09-15 18:05:16 +0300

    - getRendererByLayerName

--- ui_files/__init__.py
+++ ui_files/__init__.py
@@ -1,6 +1,5 @@
 # flake8:noqa
 from QGIS_VDO.ui_files.AnimatedGroupBox import AnimatedGroupBox
-from QGIS_VDO.ui_files.drawing import (_DrawArea,
-                                       _DrawPacketAreas,
-                                       getRendererByLayerName)
+from QGIS_VDO.ui_files.drawing import (_DrawRectangleArea,
+                                       _DrawPacketAreas)
 from QGIS_VDO.ui_files.ClickCoordinatesTool import ClickCoordinatesTool
